3b08b32c7087116277aa84cf6b1a0dc447c8369f
Aaron Grisez <acgrisez@gmail.com>
2020-09-12 14:31:57 -0700

    Refactoring to just backend and cleaning up instrumentation

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, APIRouter
 from api.managers.message import MessageManager
 from api.middlewares.tracing import OpentracingMiddleware
 from api.middlewares.debugging import DebuggingMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from api.routers import root
 import logging
 
@@ -9,6 +10,18 @@ logger = logging.getLogger("api")
 logger.setLevel(logging.DEBUG)
 app = FastAPI()
 app.include_router(root.router)
+
+origins = [
+    "https://pensive-yonath-fc0efe.netlify.app/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup():
--- api/routers/root.py
+++ api/routers/root.py
@@ -25,3 +25,7 @@ async def websocket_endpoint(websocket: WebSocket, notifier: MessageManager = De
 @router.post("/backend/push/")
 async def push_to_connected_websockets(events: SynthesizedEvent, notifier: MessageManager = Depends(get_message_manager)):
     await notifier.push(events)
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to MagentaRapids!"}
